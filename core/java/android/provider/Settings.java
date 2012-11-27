929dfeae9b009a4a4658b7bf208af68f2e046ec4
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-11-27 14:09:18 -0500

    Port "Option to hide Adb notification icon (2/2)"
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Conflicts:
    	core/res/res/values/custom_strings.xml
    	core/res/res/values/custom_symbols.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6540,6 +6540,12 @@ public final class Settings {
         public static final String QS_TILES = "sysui_qs_tiles";
 
         /**
+         * Whether to display the ADB notification.
+         * @hide
+         */
+        public static final String ADB_NOTIFY = "adb_notify";
+
+        /**
          * Whether to show four tiles per row.
          * @hide
          */
