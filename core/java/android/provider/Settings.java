a8adc6486e757986f15f656a4ab8a98a31699209
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-04 02:17:27 +0000

    Framework: Forward port ADB over network (Part 2 of 2)
    Includes:
    - ADB Over Network, integration of the adb-host mode (already present)
      The feature can be used/tested without the Settings part:
    
        setprop service.adb.tcp.port 5555
    
      Note: This ADB setting is not persistent (for security purpose) and
      require init.rc implementation event like this : http://bit.ly/AdbTcpIP
    
      Author: Tanguy Pruvot
      Id: I5c61a53948349c785356cb5aae165110d75e3074
    
      Author: sssemil <suleymanovemil8@gmail.com>
      Show notification on adb over network too
    
      Screenshots - http://goo.gl/TgsRI6
      Id: I9ddc0aa9a4f330a06ab5d97a8645d1b31bb6f299
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/purenexus_strings.xml
    	core/res/res/values/purenexus_symbols.xml

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6546,6 +6546,12 @@ public final class Settings {
         public static final String ADB_NOTIFY = "adb_notify";
 
         /**
+         * The TCP/IP port to run ADB on, or -1 for USB
+         * @hide
+         */
+        public static final String ADB_PORT = "adb_port";
+
+        /**
          * Whether to show four tiles per row.
          * @hide
          */
