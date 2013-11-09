19e1dbe905a2103da38157eda2232be4d829fa74
Lars Greiss <kufikugel@googlemail.com>
2013-11-09 18:11:27 +0100

    fb: add back all keyboard IME features back (1/2)
    this commit adds:
    
    - ime switcher notification - thanks cm
    - slims fullscreen disable on landscape
    - slims force autorotation on IME
    - volume button control for navigate in the message - thanks cm
    - slims force to show always enter key
    
    some fixes to the 4.2/4.3 implementations + added full multiuser
    support for all this features
    
    cheers
    
    Change-Id: I71e93c39620cc171e658b2d7e26491bac97c651d
    
    Conflicts:
    	core/java/android/inputmethodservice/InputMethodService.java
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/menu/sidebar_popup_menu.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java

--- packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
@@ -58,6 +58,7 @@ public class CommandQueue extends IStatusBar.Stub {
     private static final int MSG_NOTIFICATION_LIGHT_PULSE   = 17 << MSG_SHIFT;
     private static final int MSG_SHOW_SCREEN_PIN_REQUEST    = 18 << MSG_SHIFT;
     private static final int MSG_HIDE_HEADS_UP              = 19 << MSG_SHIFT;
+    private static final int MSG_SET_AUTOROTATE_STATUS      = 20 << MSG_SHIFT;
 
     public static final int FLAG_EXCLUDE_NONE = 0;
     public static final int FLAG_EXCLUDE_SEARCH_PANEL = 1 << 0;
@@ -101,6 +102,7 @@ public class CommandQueue extends IStatusBar.Stub {
         public void notificationLightPulse(int argb, int onMillis, int offMillis);
         public void showScreenPinningRequest();
         public void scheduleHeadsUpClose();
+        public void setAutoRotate(boolean enabled);
     }
 
     public CommandQueue(Callbacks callbacks, StatusBarIconList list) {
@@ -252,6 +254,14 @@ public class CommandQueue extends IStatusBar.Stub {
         synchronized (mList) {
             mHandler.removeMessages(MSG_HIDE_HEADS_UP);
             mHandler.sendEmptyMessage(MSG_HIDE_HEADS_UP);
+		}
+	}
+
+    public void setAutoRotate(boolean enabled) {
+        synchronized (mList) {
+            mHandler.removeMessages(MSG_SET_AUTOROTATE_STATUS);
+            mHandler.obtainMessage(MSG_SET_AUTOROTATE_STATUS,
+                enabled ? 1 : 0, 0, null).sendToTarget();
         }
     }
 
@@ -339,6 +349,8 @@ public class CommandQueue extends IStatusBar.Stub {
                     break;
                 case MSG_HIDE_HEADS_UP:
                     mCallbacks.scheduleHeadsUpClose();
+                case MSG_SET_AUTOROTATE_STATUS:
+                    mCallbacks.setAutoRotate(msg.arg1 != 0);
                     break;
             }
         }
