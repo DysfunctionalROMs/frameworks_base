657f43090a47f1756d7a383a323645eee5e09106
tingna_sung <tingna_sung@htc.com>
2014-10-21 15:26:38 +0800

    Not include fullscreen opaque window on screenshot
    Extend the fullscreen meaning by adding opaque drawn check on
    screenshot application; if there is a fullscreen window but not
    opaque, we will continue finding behind windows; otherwise ending up
    including behind windows. It's able to help avoiding capturing BLACK
    screenshot bitmap.
    
    Change-Id: I1c7e8f513c366688ea8001c3d0c799f3522f0ffd
    Signed-off-by: tingna_sung <tingna_sung@htc.com>

--- services/core/java/com/android/server/wm/WindowManagerService.java
+++ services/core/java/com/android/server/wm/WindowManagerService.java
@@ -6267,6 +6267,10 @@ public class WindowManagerService extends IWindowManager.Stub
                             ws.isDisplayedLw()) {
                         screenshotReady = true;
                     }
+
+                    if (ws.isFullscreen(dw, dh) && ws.isOpaqueDrawn()){
+                        break;
+                    }
                 }
 
                 if (appToken != null && appWin == null) {
