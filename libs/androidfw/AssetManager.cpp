60f47868eb3be6912a065e2e9aacfc79f19dbc86
Mårten Kongstad <marten.kongstad@sonymobile.com>
2014-11-07 10:57:15 +0100

    RRO: prevent duplicate system overlays
    System overlays, ie overlays with targetPackage="android", were loaded
    twice, which caused all sorts of issues. Ensure they are only loaded
    once, which will be during Zygote initialization.
    
    Bug: 17765434
    Change-Id: Ia5064045c77f713c58fb78adc3942f6af1abdc93
    
    Conflicts:
    	libs/androidfw/AssetManager.cpp

--- libs/androidfw/AssetManager.cpp
+++ libs/androidfw/AssetManager.cpp
@@ -605,7 +605,12 @@ FileType AssetManager::getFileType(const char* fileName)
         return kFileTypeRegular;
 }
 
-bool AssetManager::appendPathToResTable(const asset_path& ap, size_t* entryIdx) const {
+bool AssetManager::appendPathToResTable(const asset_path& ap) const {
+    // skip those ap's that correspond to system overlays
+    if (ap.isSystemOverlay) {
+        return true;
+    }
+
     Asset* ass = NULL;
     ResTable* sharedRes = NULL;
     bool shared = true;
@@ -790,6 +795,7 @@ void AssetManager::addSystemOverlays(const char* pathOverlaysList,
         oap.path = String8(buf, space - buf);
         oap.type = kFileTypeRegular;
         oap.idmap = String8(space + 1, newline - space - 1);
+        oap.isSystemOverlay = true;
 
         Asset* oass = const_cast<AssetManager*>(this)->
             openNonAssetInPathLocked("resources.arsc",
