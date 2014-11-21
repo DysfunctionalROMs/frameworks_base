70894b51fe2b1c010203c71ddad6bbd053e52d6a
Roman Birg <roman@cyngn.com>
2014-11-21 14:24:47 -0800

    SystemUI: add Settings.System toggle for quick-quick settings pulldown

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2837,6 +2837,12 @@ public final class Settings {
          */
         public static final String APP_CIRCLE_BAR_SHOW_TRIGGER = "app_circle_bar_show_trigger";
 
+        /** Whether to allow one finger quick settings expansion on the right side of the statusbar.
+         *
+         * @hide
+         */
+        public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
+
         /**
          * Whether to show reboot in power menu 
          * @hide
