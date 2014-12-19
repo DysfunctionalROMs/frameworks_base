9d6723f1ce3b3772db13495746fe4906a8862c05
Dave Kessler <activethrasher00@gmail.com>
2014-12-19 18:21:01 -0500

    Power menu customizations [1/2]
    power
    reboot
    screenshot
    airplane mode
    user switcher
    bug reports
    sound modes
    assist
    voice search
    
    Beanstown106 edits:
    *adapted for android 6.0 marshmallow
    *added assist and voice search options
    
    Conflicts:
    	core/res/res/values/chroma_strings.xml
    	services/core/java/com/android/server/policy/GlobalActions.java
    	services/core/java/com/android/server/power/ShutdownThread.java
    
    Conflicts:
    	core/res/res/values/custom_strings.xml
    	core/res/res/values/custom_symbols.xml
    	services/core/java/com/android/server/policy/GlobalActions.java

--- services/core/java/com/android/server/power/ShutdownThread.java
+++ services/core/java/com/android/server/power/ShutdownThread.java
@@ -176,6 +176,25 @@ public final class ShutdownThread extends Thread {
                 resourceId = com.android.internal.R.string.shutdown_confirm_question;
             } else {
                 resourceId = com.android.internal.R.string.shutdown_confirm;
+			}
+		}
+
+        boolean showRebootOption = false;
+
+        String[] actionsArray;
+        String actions = Settings.Global.getStringForUser(context.getContentResolver(),
+                Settings.Global.POWER_MENU_ACTIONS, UserHandle.USER_CURRENT);
+        if (actions == null) {
+            actionsArray = context.getResources().getStringArray(
+                    com.android.internal.R.array.config_globalActionsList);
+        } else {
+            actionsArray = actions.split("\\|");
+        }
+
+        for (int i = 0; i < actionsArray.length; i++) {
+            if (actionsArray[i].equals("reboot")) {
+                showRebootOption = true;
+                break;
             }
 
             Log.d(TAG, "longPressBehavior=" + longPressBehavior);
