0112b33a6a754ab4f0f833e3009f137380e39c47
Gianmarco Reverberi <gianmarco.reverberi@gmail.com>
2015-01-07 19:16:06 +0100

    QS: change location sensors mode in quick settings [1/2]
    Clicking on the location tile now opens a detail view
    where we can either enable/disable location or
    switch through the location modes.
    Added a new icon for battery saving mode.
    Added a switch in notification drawer settings
    to enable this.
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/purenexus_strings.xml
    	packages/SystemUI/src/com/android/systemui/qs/tiles/LocationTile.java
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6515,6 +6515,12 @@ public final class Settings {
         public static final String CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED =
                 "camera_double_tap_power_gesture_disabled";
 
+        /**
+         * Whether detail view for the location tile is enabled
+         * @hide
+         */
+        public static final String QS_LOCATION_ADVANCED = "qs_location_advanced";
+
         /** Whether to show the brightness slider in quick settings panel.
          *
          * @hide
