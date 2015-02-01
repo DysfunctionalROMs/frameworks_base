15a3ff2750700b59f8cbd3b69ec54df8a2c26afa
BestPig <bestpig82@gmail.com>
2015-02-01 03:04:41 +0100

    SystemUI: add USB Tether tile
    Drawables by blunden
    
    Change-Id: Ic44f2b596fe32152f3f722347a855325c52113c6

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -44,6 +44,7 @@ public class QSConstants {
     public static final String TILE_AMBIENT_DISPLAY = "ambient_display";
     public static final String TILE_SCREENSHOT = "screenshot";
     public static final String TILE_ADB_NETWORK = "adb_network";
+    public static final String TILE_USB_TETHER = "usb_tether";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -78,5 +79,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_AMBIENT_DISPLAY);
         TILES_AVAILABLE.add(TILE_SCREENSHOT);
         TILES_AVAILABLE.add(TILE_ADB_NETWORK);
+		TILES_AVAILABLE.add(TILE_USB_TETHER);
     }
 }
