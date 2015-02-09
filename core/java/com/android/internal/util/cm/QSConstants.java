f3c024e16242e40a9caec5235f6a76899a839544
Scott Mertz <scott@cyngn.com>
2015-02-09 12:57:19 -0800

    [1/3] CmHardwareService: Initial implementation and a few fixes - CMHW legacy implementation is not stateful and causes many issues   with applications stomping on eachother. - Permissions/sepolicies are now contained within system_server context
    Change-Id: Ia5853a96c81f9113811eee8d9bbdf6b5db654376

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -44,14 +44,10 @@ public class QSConstants {
     public static final String TILE_APPCIRCLEBAR = "toggleAppCircleBar";
     public static final String TILE_MUSIC = "music";
     public static final String TILE_AMBIENT_DISPLAY = "ambient_display";
-<<<<<<< HEAD:core/java/com/android/internal/util/cm/QSConstants.java
     public static final String TILE_VISUALIZER = "visualizer";
     public static final String TILE_SCREEN_TIMEOUT = "screen_timeout";
     public static final String TILE_LED = "led";
-=======
-    public static final String TILE_SCREENSHOT = "screenshot";
     public static final String TILE_LIVE_DISPLAY = "live_display";
->>>>>>> a05bcc6... livedisplay: Implement active display adjustment:core/java/com/android/internal/util/slim/QSConstants.java
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
