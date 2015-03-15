ef71cc328219f98b822d2b9c6b80612f3638a244
lichti1901 <lichti190185@gmail.com>
2015-03-15 10:08:41 +0100

    Revert "systemui: never show excluded recent task in recents view"
    This reverts commit 9db21ccac4c76a0b0cc63335f0e1209f1cd70784.

--- packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/TaskStackView.java
@@ -21,8 +21,6 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.graphics.Matrix;
 import android.graphics.Rect;
-import android.os.UserHandle;
-import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -565,10 +563,6 @@ public class TaskStackView extends FrameLayout implements TaskStack.TaskStackCal
                         mStack.removeTask(t);
                     }
                 }
-
-                // And remove all the excluded or all the other tasks
-                SystemServicesProxy ssp = RecentsTaskLoader.getInstance().getSystemServicesProxy();
-                ssp.removeAllUserTask(UserHandle.myUserId());
             }
         });
     }
