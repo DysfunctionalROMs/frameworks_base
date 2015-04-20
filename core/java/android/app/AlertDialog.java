XXMrHyde/android_frameworks_base

Add dark themes for dialogs and alert dialogs:
A dark material theme with a blue grey background,
and a color accent from the light version of the material theme.

- Apply the new theme to the Power-, reboot menu and the shutdown dialog

core/java/android/app/AlertDialog.java
@@ -93,6 +93,8 @@ public class AlertDialog extends Dialog implements DialogInterface {
      */
     public static final int THEME_DEVICE_DEFAULT_LIGHT = 5;
 
+    public static final int THEME_MATERIAL_DARK = 6;
+
     /**
      * No layout hint.
      * @hide
@@ -147,6 +149,8 @@ public class AlertDialog extends Dialog implements DialogInterface {
             return com.android.internal.R.style.Theme_DeviceDefault_Dialog_Alert;
         } else if (resid == THEME_DEVICE_DEFAULT_LIGHT) {
             return com.android.internal.R.style.Theme_DeviceDefault_Light_Dialog_Alert;
+        } else if (resid == THEME_MATERIAL_DARK) {
+            return com.android.internal.R.style.Theme_Material_Dialog_Alert_DarkKat;
         } else if (resid >= 0x01000000) {   // start of real resource IDs.
             return resid;
         } else {

DysfunctionalROMs/frameworks_base

Conflicts:
	core/res/res/drawable/ic_lock_reboot_dark.xml
	core/res/res/values/dk_symbols.xml

core/java/android/app/AlertDialog.java
@@ -93,6 +93,8 @@ public class AlertDialog extends Dialog implements DialogInterface {
      */
     public static final int THEME_DEVICE_DEFAULT_LIGHT = 5;
 
+    public static final int THEME_MATERIAL_DARK = 6;
+
     /**
      * No layout hint.
      * @hide
@@ -147,6 +149,8 @@ public class AlertDialog extends Dialog implements DialogInterface {
             return com.android.internal.R.style.Theme_DeviceDefault_Dialog_Alert;
         } else if (resid == THEME_DEVICE_DEFAULT_LIGHT) {
             return com.android.internal.R.style.Theme_DeviceDefault_Light_Dialog_Alert;
+        } else if (resid == THEME_MATERIAL_DARK) {
+            return com.android.internal.R.style.Theme_Material_Dialog_Alert_Broken;
         } else if (resid >= 0x01000000) {   // start of real resource IDs.
             return resid;
         } else {