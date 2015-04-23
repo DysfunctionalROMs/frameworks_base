XXMrHyde/android_frameworks_base

Status bar: Custom notification- and system icon color, (WIP), (1/2):
- Option to colorize notification icons
- Option to show/hide the ticker
- Option to show/hide the notification count icon
- Cutom icon color
- Custom ticker text color
- custom notification count icon color
- custom notification count text color

Thanks to SlimRoms for the ticker option:
https://github.com/SlimRoms/frameworks_base/commit/9766c0e9c4465f674cb0729b24fa6fa1ed92e7ec
https://github.com/SlimRoms/frameworks_base/commit/4d94be5d45fcc2bdc1d5f7306e864fba6f2b02cf

Bugs:
- Some icons don`t respect the colorize notification icons option

core/java/com/android/internal/util/darkkat/ImageHelper.java
@@ -1,0 +1,131 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.darkkat;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Shader.TileMode;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.TypedValue;
+
+public class ImageHelper {
+
+    public static Bitmap getColoredBitmap(Drawable d, int color) {
+        if (d == null) {
+            return null;
+        }
+        Bitmap colorBitmap = ((BitmapDrawable) d).getBitmap();
+        Bitmap grayscaleBitmap = toGrayscale(colorBitmap);
+        Paint pp = new Paint();
+        pp.setAntiAlias(true);
+        PorterDuffColorFilter frontFilter =
+            new PorterDuffColorFilter(color, Mode.MULTIPLY);
+        pp.setColorFilter(frontFilter);
+        Canvas cc = new Canvas(grayscaleBitmap);
+        final Rect rect = new Rect(0, 0, grayscaleBitmap.getWidth(), grayscaleBitmap.getHeight());
+        cc.drawBitmap(grayscaleBitmap, rect, rect, pp);
+        return grayscaleBitmap;
+    }
+
+    private static Bitmap toGrayscale(Bitmap bmpOriginal) {
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+
+        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmpGrayscale);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        ColorMatrix cm = new ColorMatrix();
+        final Rect rect = new Rect(0, 0, width, height);
+        cm.setSaturation(0);
+
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, rect, rect, paint);
+        return bmpGrayscale;
+    }
+
+    public static Drawable resize(Context context, Drawable image, int size) {
+        if (image == null || context == null) {
+            return null;
+        }
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, size,
+                context.getResources().getDisplayMetrics());
+
+        Bitmap d = ((BitmapDrawable) image).getBitmap();
+        Bitmap bitmapOrig = Bitmap.createScaledBitmap(d, px, px, true);
+        return new BitmapDrawable(context.getResources(), bitmapOrig);
+    }
+
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
+        if (bitmap == null) {
+            return null;
+        }
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(),
+                Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = 24;
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+
+    public static Bitmap getCircleBitmap(Bitmap bitmap) {
+        if (bitmap == null) {
+            return null;
+        }
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+
+        Bitmap output = Bitmap.createBitmap(width, height,
+                Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        BitmapShader shader = new BitmapShader(bitmap,  TileMode.CLAMP, TileMode.CLAMP);
+        final Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setShader(shader);
+
+        canvas.drawCircle(width/2, height/2, width/2, paint);
+
+        return output;
+    }
+
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/Ticker.java
@@ -16,10 +16,15 @@
 
 package com.android.systemui.statusbar.phone;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.DrawableContainer;
+import android.graphics.PorterDuff.Mode;
 import android.os.Handler;
+import android.provider.Settings;
 import android.service.notification.StatusBarNotification;
 import android.text.Layout.Alignment;
 import android.text.StaticLayout;
@@ -31,6 +36,7 @@ import android.widget.TextSwitcher;
 import android.widget.TextView;
 
 import com.android.internal.statusbar.StatusBarIcon;
+import com.android.internal.util.darkkat.ImageHelper;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.StatusBarIconView;
 
@@ -47,6 +53,7 @@ public abstract class Ticker {
     private ImageSwitcher mIconSwitcher;
     private TextSwitcher mTextSwitcher;
     private float mIconScale;
+    private int mNotifTextColor;
 
     public static boolean isGraphicOrEmoji(char c) {
         int gc = Character.getType(c);
@@ -176,11 +183,13 @@ public abstract class Ticker {
         // Copy the paint style of one of the TextSwitchers children to use later for measuring
         TextView text = (TextView)mTextSwitcher.getChildAt(0);
         mPaint = text.getPaint();
+        updateTextColor();
     }
 
 
     public void addEntry(StatusBarNotification n) {
         int initialCount = mSegments.size();
+        ContentResolver resolver = mContext.getContentResolver();
 
         // If what's being displayed has the same text and icon, just drop it
         // (which will let the current one finish, this happens when apps do
@@ -201,6 +210,11 @@ public abstract class Ticker {
                         n.getNotification().tickerText));
         final CharSequence text = n.getNotification().tickerText;
         final Segment newSegment = new Segment(n, icon, text);
+        boolean colorizeNotifIcons = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_COLORIZE_NOTIF_ICONS, 0) == 1;
+        int iconColor = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_NOTIF_SYSTEM_ICON_COLOR,
+                0xffffffff);
 
         // If there's already a notification schedule for this package and id, remove it.
         for (int i=0; i<mSegments.size(); i++) {
@@ -219,11 +233,25 @@ public abstract class Ticker {
 
             mIconSwitcher.setAnimateFirstView(false);
             mIconSwitcher.reset();
-            mIconSwitcher.setImageDrawable(seg.icon);
+            if (colorizeNotifIcons) {
+                if (seg.icon instanceof AnimationDrawable) {
+                    ((DrawableContainer)seg.icon).setColorFilter(iconColor,
+                           Mode.MULTIPLY);
+
+                    mIconSwitcher.setImageDrawable(seg.icon);
+                } else {
+                    mIconSwitcher.setImageBitmap(ImageHelper
+                            .getColoredBitmap(seg.icon, iconColor));
+                }
+            } else {
+                mIconSwitcher.setImageDrawable(seg.icon);
+            }
 
             mTextSwitcher.setAnimateFirstView(false);
             mTextSwitcher.reset();
             mTextSwitcher.setText(seg.getText());
+            updateTextColor();
+            mTextSwitcher.setTextColor(mNotifTextColor);
 
             tickerStarting();
             scheduleAdvance();
@@ -264,6 +292,8 @@ public abstract class Ticker {
             Segment seg = mSegments.get(0);
             CharSequence text = seg.getText();
             mTextSwitcher.setCurrentText(text);
+            updateTextColor();
+            mTextSwitcher.setTextColor(mNotifTextColor);
         }
     }
 
@@ -284,6 +314,8 @@ public abstract class Ticker {
                     continue;
                 }
                 mTextSwitcher.setText(text);
+                updateTextColor();
+                mTextSwitcher.setTextColor(mNotifTextColor);
 
                 scheduleAdvance();
                 break;
@@ -301,5 +333,13 @@ public abstract class Ticker {
     public abstract void tickerStarting();
     public abstract void tickerDone();
     public abstract void tickerHalting();
+
+    public void updateTextColor() {
+        ContentResolver resolver = mContext.getContentResolver();
+
+        mNotifTextColor = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_NOTIF_TEXT_COLOR,
+                0xffffffff);
+    }
 }
 

DysfunctionalROMs/frameworks_base

Conflicts:
	core/java/android/provider/Settings.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

core/java/com/android/internal/util/broken/ImageHelper.java
@@ -1,0 +1,131 @@
+/*
+* Copyright (C) 2013 SlimRoms Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.broken;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Shader.TileMode;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.TypedValue;
+
+public class ImageHelper {
+
+    public static Bitmap getColoredBitmap(Drawable d, int color) {
+        if (d == null) {
+            return null;
+        }
+        Bitmap colorBitmap = ((BitmapDrawable) d).getBitmap();
+        Bitmap grayscaleBitmap = toGrayscale(colorBitmap);
+        Paint pp = new Paint();
+        pp.setAntiAlias(true);
+        PorterDuffColorFilter frontFilter =
+            new PorterDuffColorFilter(color, Mode.MULTIPLY);
+        pp.setColorFilter(frontFilter);
+        Canvas cc = new Canvas(grayscaleBitmap);
+        final Rect rect = new Rect(0, 0, grayscaleBitmap.getWidth(), grayscaleBitmap.getHeight());
+        cc.drawBitmap(grayscaleBitmap, rect, rect, pp);
+        return grayscaleBitmap;
+    }
+
+    private static Bitmap toGrayscale(Bitmap bmpOriginal) {
+        int width, height;
+        height = bmpOriginal.getHeight();
+        width = bmpOriginal.getWidth();
+
+        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(bmpGrayscale);
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        ColorMatrix cm = new ColorMatrix();
+        final Rect rect = new Rect(0, 0, width, height);
+        cm.setSaturation(0);
+
+        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+        paint.setColorFilter(f);
+        c.drawBitmap(bmpOriginal, rect, rect, paint);
+        return bmpGrayscale;
+    }
+
+    public static Drawable resize(Context context, Drawable image, int size) {
+        if (image == null || context == null) {
+            return null;
+        }
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, size,
+                context.getResources().getDisplayMetrics());
+
+        Bitmap d = ((BitmapDrawable) image).getBitmap();
+        Bitmap bitmapOrig = Bitmap.createScaledBitmap(d, px, px, true);
+        return new BitmapDrawable(context.getResources(), bitmapOrig);
+    }
+
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap) {
+        if (bitmap == null) {
+            return null;
+        }
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(),
+                Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        final float roundPx = 24;
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+
+    public static Bitmap getCircleBitmap(Bitmap bitmap) {
+        if (bitmap == null) {
+            return null;
+        }
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+
+        Bitmap output = Bitmap.createBitmap(width, height,
+                Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        BitmapShader shader = new BitmapShader(bitmap,  TileMode.CLAMP, TileMode.CLAMP);
+        final Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setShader(shader);
+
+        canvas.drawCircle(width/2, height/2, width/2, paint);
+
+        return output;
+    }
+
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/Ticker.java
@@ -16,10 +16,15 @@
 
 package com.android.systemui.statusbar.phone;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.DrawableContainer;
+import android.graphics.PorterDuff.Mode;
 import android.os.Handler;
+import android.provider.Settings;
 import android.service.notification.StatusBarNotification;
 import android.text.Layout.Alignment;
 import android.text.StaticLayout;
@@ -31,6 +36,7 @@ import android.widget.TextSwitcher;
 import android.widget.TextView;
 
 import com.android.internal.statusbar.StatusBarIcon;
+import com.android.internal.util.broken.ImageHelper;
 import com.android.systemui.R;
 import com.android.systemui.statusbar.StatusBarIconView;
 
@@ -47,6 +53,7 @@ public abstract class Ticker {
     private ImageSwitcher mIconSwitcher;
     private TextSwitcher mTextSwitcher;
     private float mIconScale;
+    private int mNotifTextColor;
 
     public static boolean isGraphicOrEmoji(char c) {
         int gc = Character.getType(c);
@@ -176,11 +183,13 @@ public abstract class Ticker {
         // Copy the paint style of one of the TextSwitchers children to use later for measuring
         TextView text = (TextView)mTextSwitcher.getChildAt(0);
         mPaint = text.getPaint();
+        updateTextColor();
     }
 
 
     public void addEntry(StatusBarNotification n) {
         int initialCount = mSegments.size();
+        ContentResolver resolver = mContext.getContentResolver();
 
         // If what's being displayed has the same text and icon, just drop it
         // (which will let the current one finish, this happens when apps do
@@ -201,6 +210,11 @@ public abstract class Ticker {
                         n.getNotification().tickerText));
         final CharSequence text = n.getNotification().tickerText;
         final Segment newSegment = new Segment(n, icon, text);
+        boolean colorizeNotifIcons = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_COLORIZE_NOTIF_ICONS, 0) == 1;
+        int iconColor = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_NOTIF_SYSTEM_ICON_COLOR,
+                0xffffffff);
 
         // If there's already a notification schedule for this package and id, remove it.
         for (int i=0; i<mSegments.size(); i++) {
@@ -219,11 +233,25 @@ public abstract class Ticker {
 
             mIconSwitcher.setAnimateFirstView(false);
             mIconSwitcher.reset();
-            mIconSwitcher.setImageDrawable(seg.icon);
+            if (colorizeNotifIcons) {
+                if (seg.icon instanceof AnimationDrawable) {
+                    ((DrawableContainer)seg.icon).setColorFilter(iconColor,
+                           Mode.MULTIPLY);
+
+                    mIconSwitcher.setImageDrawable(seg.icon);
+                } else {
+                    mIconSwitcher.setImageBitmap(ImageHelper
+                            .getColoredBitmap(seg.icon, iconColor));
+                }
+            } else {
+                mIconSwitcher.setImageDrawable(seg.icon);
+            }
 
             mTextSwitcher.setAnimateFirstView(false);
             mTextSwitcher.reset();
             mTextSwitcher.setText(seg.getText());
+            updateTextColor();
+            mTextSwitcher.setTextColor(mNotifTextColor);
 
             tickerStarting();
             scheduleAdvance();
@@ -264,6 +292,8 @@ public abstract class Ticker {
             Segment seg = mSegments.get(0);
             CharSequence text = seg.getText();
             mTextSwitcher.setCurrentText(text);
+            updateTextColor();
+            mTextSwitcher.setTextColor(mNotifTextColor);
         }
     }
 
@@ -284,6 +314,8 @@ public abstract class Ticker {
                     continue;
                 }
                 mTextSwitcher.setText(text);
+                updateTextColor();
+                mTextSwitcher.setTextColor(mNotifTextColor);
 
                 scheduleAdvance();
                 break;
@@ -301,5 +333,13 @@ public abstract class Ticker {
     public abstract void tickerStarting();
     public abstract void tickerDone();
     public abstract void tickerHalting();
+
+    public void updateTextColor() {
+        ContentResolver resolver = mContext.getContentResolver();
+
+        mNotifTextColor = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_NOTIF_TEXT_COLOR,
+                0xffffffff);
+    }
 }
 