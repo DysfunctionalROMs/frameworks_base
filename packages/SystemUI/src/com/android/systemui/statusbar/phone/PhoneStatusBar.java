faa1732f851ad019c93c6f84ed721dde9f383de6
FuckingShitUp <kengreenwood420666@gmail.com>
2015-04-25 23:31:59 -0500

    Revert "Return: Smart Pulldown [1/2]"
    This reverts commit a7885cc3193cb6b338769d1ea3d0c77ba281398b.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -1918,14 +1918,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
     }
 
-    protected boolean hasActiveVisibleNotifications() {
-        return mNotificationData.hasActiveVisibleNotifications();
-    }
-
-    protected boolean hasActiveClearableNotifications() {
-        return mNotificationData.hasActiveClearableNotifications();
-    }
-
     @Override
     protected void setAreThereNotifications() {
 
