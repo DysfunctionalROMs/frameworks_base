4e14045dfe7d722eb5c90dc499a8aef88a71f517
FuckingShitUp <kengreenwood420666@gmail.com>
2015-04-29 16:29:38 -0500

    Revert "Switch to new battery implementation"
    This reverts commit 43dacc2aa215b943918e0984c06e49b5ac83dccf.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3645,7 +3645,7 @@ public final class Settings {
             RINGTONE,
             LOCK_TO_APP_ENABLED,
             NOTIFICATION_SOUND,
-            STATUS_BAR_SHOW_BATTERY_PERCENT
+            STATUS_BAR_SHOW_BATTERY_PERCENT,
         };
 
         /**
