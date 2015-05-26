ba331ba430da1d8760cef0e52955a3001005bdc2
Leon Scroggins III <scroggo@google.com>
2015-05-26 16:41:09 -0400

    Check that the parcel contained the expected amount of region data. DO NOT MERGE
    bug:20883006
    Change-Id: Ib47a8ec8696dbc37e958b8dbceb43fcbabf6605b
    
    Conflicts:
    	core/jni/android/graphics/Region.cpp

--- core/jni/android/graphics/Region.cpp
+++ core/jni/android/graphics/Region.cpp
@@ -212,13 +212,13 @@ static jlong Region_createFromParcel(JNIEnv* env, jobject clazz, jobject parcel)
 
     android::Parcel* p = android::parcelForJavaObject(env, parcel);
 
-    SkRegion* region = new SkRegion;
-    size_t actualSize = region->readFromMemory(regionData, size);
-
-    if (size != actualSize) {
-        delete region;
+    const size_t size = p->readInt32();
+    const void* regionData = p->readInplace(size);
+    if (regionData == NULL) {
         return NULL;
     }
+    SkRegion* region = new SkRegion;
+    region->readFromMemory(regionData, size);
 
     return reinterpret_cast<jlong>(region);
 }
