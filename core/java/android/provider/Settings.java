c4944a65f6fff172a857d3027956b9fe301e010d
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-06-01 14:33:44 +0100

    SystemUI: Double tap to sleep on the lock screen (1/2)
    Double tap on the top third or the bottom shortcut area to sleep
    
    Change-Id: I848161bca30e58009ade21cbba3e5e1672ba85ac

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3655,6 +3655,12 @@ public final class Settings {
          */
         public static final String DOUBLE_TAP_SLEEP_NAVBAR = "double_tap_sleep_navbar";
 
+         /**
+         * Enable double tap gesture anywhere on the lock screen put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_LOCK_SCREEN = "double_tap_sleep_lock_screen";
+
         /**
          * Config for advanced pa pie 2.0
          *
