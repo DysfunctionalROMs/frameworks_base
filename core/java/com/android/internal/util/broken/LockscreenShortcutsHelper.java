XXMrHyde/android_frameworks_base

Lockscreen shortcut customizations, (1/2):
Based on Cyanogenmod implementation:

SettingsProvider : Add functions for delimited strings:
https://github.com/CyanogenMod/android_frameworks_base/commit/8509a09c0ca9e11410c3cdeade61926cb413f5fc

SystemUI: Lockscreen shortcut customization:
https://github.com/CyanogenMod/android_frameworks_base/commit/01ac1922e058d340670061018cfc005c04fca387

SystemUI : Fix Rtl for lockscreen shortcuts:
https://github.com/CyanogenMod/android_frameworks_base/commit/35faff948989c5e96f349612aba16a03f3f5d13b

SystemUI : Add ability to disable lockscreen targets:
https://github.com/CyanogenMod/android_frameworks_base/commit/241683a24e09dbd6e53776620c07b488fcf71523

Additions:
- Icons are using the lock screen icon color, (custom targets optional)
- Removed the saturation color filter for custom target icons
- Option to colorize all icons:
  - Disabled:
    - Custom target icons are shown without any modifications
    - Custom target touch feedbacks are using the pallete color of the icon, (no change)
  - Enabled:
    - Custom target icons and touch feedback are using the lock screen icon color

core/java/com/android/internal/util/darkkat/LockscreenShortcutsHelper.java
@@ -1,0 +1,233 @@
+package com.android.internal.util.darkkat;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.Settings;
+import android.text.TextUtils;
+
+import com.android.internal.util.darkkat.ImageHelper;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LockscreenShortcutsHelper {
+
+    public enum Shortcuts {
+        LEFT_SHORTCUT(0),
+        RIGHT_SHORTCUT(1);
+
+        private final int index;
+
+        private Shortcuts(int index) {
+            this.index = index;
+        }
+    }
+
+    public static final String DEFAULT = "default";
+    public static final String NONE = "none";
+    private static final String DELIMITER = "|";
+    private static final String SYSTEM_UI_PKGNAME = "com.android.systemui";
+    private static final String PHONE_DEFAULT_ICON = "ic_phone_24dp";
+    private static final String CAMERA_DEFAULT_ICON = "ic_camera_alt_24dp";
+
+    private final Context mContext;
+    private Resources mSystemUiResources;
+    private OnChangeListener mListener;
+    private List<String> mTargetActivities;
+    private boolean mColorizeCustomIcons;
+    private int mIconColor;
+
+    public interface OnChangeListener {
+        public void onChange();
+    }
+
+    public LockscreenShortcutsHelper(Context context, OnChangeListener listener) {
+        mContext = context;
+        if (listener != null) {
+            mListener = listener;
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_TARGETS), false, mObserver);
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(
+                    Settings.System.LOCK_SCREEN_SHORTCUTS_COLORIZE_CUSTOM_ICONS),
+                    false, mObserver);
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(
+                    Settings.System.LOCK_SCREEN_ICON_COLOR), false, mObserver);
+        }
+        fetchTargets();
+    }
+
+    private ContentObserver mObserver = new ContentObserver(null) {
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            fetchTargets();
+            if (mListener != null) {
+                mListener.onChange();
+            }
+        }
+    };
+
+    public static class TargetInfo {
+        public Drawable icon;
+        public boolean colorizeCustomIcons;
+        public String uri;
+        public TargetInfo(Drawable icon, boolean colorizeCustomIcons, String uri) {
+            this.icon = icon;
+            this.colorizeCustomIcons = colorizeCustomIcons;
+            this.uri = uri;
+        }
+    }
+
+    private void fetchTargets() {
+        mTargetActivities = Settings.System.getDelimitedStringAsList(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_TARGETS, DELIMITER);
+        ContentResolver resolver = mContext.getContentResolver();
+        int itemsToPad = Shortcuts.values().length - mTargetActivities.size();
+        if (itemsToPad > 0) {
+            for (int i = 0; i < itemsToPad; i++) {
+                mTargetActivities.add(DEFAULT);
+            }
+        }
+        mColorizeCustomIcons = Settings.System.getInt(resolver,
+                Settings.System.LOCK_SCREEN_SHORTCUTS_COLORIZE_CUSTOM_ICONS, 0) == 1;
+        mIconColor = Settings.System.getInt(resolver,
+                Settings.System.LOCK_SCREEN_ICON_COLOR, 0xffffffff);
+    }
+
+    public List<TargetInfo> getDrawablesForTargets() {
+        fetchTargets();
+        List<TargetInfo> result = new ArrayList<TargetInfo>();
+
+        for (int i = 0; i < Shortcuts.values().length; i++) {
+            String activity = mTargetActivities.get(i);
+            Drawable drawable = null;
+
+            if (!TextUtils.isEmpty(activity) && !activity.equals(NONE)) {
+                // No pre-defined action, try to resolve URI
+                try {
+                    Intent intent = Intent.parseUri(activity, 0);
+                    PackageManager pm = mContext.getPackageManager();
+                    ActivityInfo info = intent.resolveActivityInfo(pm,
+                            PackageManager.GET_ACTIVITIES);
+
+                    if (info != null) {
+                        Drawable d = info.loadIcon(pm);
+                        if (mColorizeCustomIcons) {
+                            drawable = new BitmapDrawable(ImageHelper.getColoredBitmap(d, mIconColor));
+                        } else {
+                            drawable = d;
+                        }
+                    }
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                    // Treat as empty
+                }
+            }
+
+            if (drawable == null) {
+                drawable = getDrawableFromSystemUI(i == Shortcuts.LEFT_SHORTCUT.index
+                        ? PHONE_DEFAULT_ICON : CAMERA_DEFAULT_ICON);
+            }
+
+            result.add(new TargetInfo(drawable, mColorizeCustomIcons, activity));
+        }
+        return result;
+    }
+
+    public Drawable getDrawableFromSystemUI(String name) {
+        Resources res;
+        if (mContext.getPackageName().equals(SYSTEM_UI_PKGNAME)) {
+            res = mContext.getResources();
+        } else {
+            if (mSystemUiResources == null) {
+                try {
+                    PackageManager pm = mContext.getPackageManager();
+                    mSystemUiResources = pm.getResourcesForApplication(SYSTEM_UI_PKGNAME);
+                } catch (PackageManager.NameNotFoundException e) {
+                    // ignore
+                }
+            }
+            res = mSystemUiResources;
+        }
+        if (res == null) {
+            return null;
+        }
+        int id = res.getIdentifier(name, "drawable", SYSTEM_UI_PKGNAME);
+        return id > 0 ? res.getDrawable(id) : null;
+    }
+
+    private String getFriendlyActivityName(Intent intent, boolean labelOnly) {
+        PackageManager pm = mContext.getPackageManager();
+        ActivityInfo ai = intent.resolveActivityInfo(pm, PackageManager.GET_ACTIVITIES);
+        String friendlyName = null;
+        if (ai != null) {
+            friendlyName = ai.loadLabel(pm).toString();
+            if (friendlyName == null && !labelOnly) {
+                friendlyName = ai.name;
+            }
+        }
+        return friendlyName != null || labelOnly ? friendlyName : intent.toUri(0);
+    }
+
+    private String getFriendlyShortcutName(Intent intent) {
+        String activityName = getFriendlyActivityName(intent, true);
+        String name = intent.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
+
+        if (activityName != null && name != null) {
+            return activityName + ": " + name;
+        }
+        return name != null ? name : intent.toUri(0);
+    }
+
+    public String getFriendlyNameForUri(Shortcuts shortcut) {
+        Intent intent = getIntent(shortcut);
+        if (Intent.ACTION_MAIN.equals(intent.getAction())) {
+            return getFriendlyActivityName(intent, false);
+        }
+        return getFriendlyShortcutName(intent);
+    }
+
+    public boolean isTargetCustom(Shortcuts shortcut) {
+        if (mTargetActivities == null || mTargetActivities.isEmpty()) {
+            return false;
+        }
+        String action = mTargetActivities.get(shortcut.index);
+        if (DEFAULT.equals(action)) {
+            return false;
+        }
+
+        return NONE.equals(action) || getIntent(shortcut) != null;
+    }
+
+    public boolean isTargetEmpty(Shortcuts shortcut) {
+        return mTargetActivities != null &&
+                !mTargetActivities.isEmpty() &&
+                mTargetActivities.get(shortcut.index).equals(NONE);
+    }
+
+    public Intent getIntent(Shortcuts shortcut) {
+        Intent intent = null;
+        try {
+            intent = Intent.parseUri(mTargetActivities.get(shortcut.index), 0);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return intent;
+    }
+
+    public void saveTargets(ArrayList<String> targets) {
+        Settings.System.putListAsDelimitedString(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_TARGETS, DELIMITER, targets);
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -45,6 +45,7 @@ import android.view.animation.Interpolator;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.android.internal.util.darkkat.LockscreenShortcutsHelper;
 import com.android.keyguard.KeyguardStatusView;
 import com.android.systemui.R;
 import com.android.systemui.qs.QSPanel;
@@ -1233,8 +1234,8 @@ public class NotificationPanelView extends PanelView implements
 
         final float w = getMeasuredWidth();
         float region = (w * (1.f/3.f)); // TODO overlay region fraction?
-        final boolean showQsOverride = isLayoutRtl() ? (x < region) : (w - region < x)
-                 && mStatusBarState == StatusBarState.SHADE;
+        boolean showQsOverride = isLayoutRtl() ? (x < region) : (w - region < x);
+        showQsOverride = showQsOverride && mStatusBarState == StatusBarState.SHADE;
 
         if (mQsExpanded) {
             return onHeader || (mScrollView.isScrolledToBottom() && yDiff < 0) && isInQsArea(x, y);
@@ -1589,8 +1590,11 @@ public class NotificationPanelView extends PanelView implements
         mLaunchAnimationEndRunnable = null;
         if (start) {
             mKeyguardBottomArea.launchPhone();
-        } else {
+        } else if (!mKeyguardBottomArea.isTargetCustom(
+                LockscreenShortcutsHelper.Shortcuts.RIGHT_SHORTCUT)) {
             mSecureCameraLaunchManager.startSecureCameraLaunch();
+        } else {
+            mKeyguardBottomArea.launchCamera();
         }
         mBlockTouches = true;
     }
@@ -1622,9 +1626,9 @@ public class NotificationPanelView extends PanelView implements
         });
         boolean start = getLayoutDirection() == LAYOUT_DIRECTION_RTL ? right : !right;
         if (start) {
-            mStatusBar.onPhoneHintStarted();
+            mStatusBar.onPhoneHintStarted(mKeyguardBottomArea.getLeftHint());
         } else {
-            mStatusBar.onCameraHintStarted();
+            mStatusBar.onCameraHintStarted(mKeyguardBottomArea.getRightHint());
         }
     }
 

DysfunctionalROMs/frameworks_base

Conflicts:
	packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
	packages/SystemUI/Android.mk
	packages/SystemUI/res/values-de/dk_strings.xml
	packages/SystemUI/res/values/dk_strings.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardBottomAreaView.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

core/java/com/android/internal/util/broken/LockscreenShortcutsHelper.java
@@ -1,0 +1,233 @@
+package com.android.internal.util.broken;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.database.ContentObserver;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.provider.Settings;
+import android.text.TextUtils;
+
+import com.android.internal.util.broken.ImageHelper;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LockscreenShortcutsHelper {
+
+    public enum Shortcuts {
+        LEFT_SHORTCUT(0),
+        RIGHT_SHORTCUT(1);
+
+        private final int index;
+
+        private Shortcuts(int index) {
+            this.index = index;
+        }
+    }
+
+    public static final String DEFAULT = "default";
+    public static final String NONE = "none";
+    private static final String DELIMITER = "|";
+    private static final String SYSTEM_UI_PKGNAME = "com.android.systemui";
+    private static final String PHONE_DEFAULT_ICON = "ic_phone_24dp";
+    private static final String CAMERA_DEFAULT_ICON = "ic_camera_alt_24dp";
+
+    private final Context mContext;
+    private Resources mSystemUiResources;
+    private OnChangeListener mListener;
+    private List<String> mTargetActivities;
+    private boolean mColorizeCustomIcons;
+    private int mIconColor;
+
+    public interface OnChangeListener {
+        public void onChange();
+    }
+
+    public LockscreenShortcutsHelper(Context context, OnChangeListener listener) {
+        mContext = context;
+        if (listener != null) {
+            mListener = listener;
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(
+                    Settings.System.LOCKSCREEN_TARGETS), false, mObserver);
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(
+                    Settings.System.LOCK_SCREEN_SHORTCUTS_COLORIZE_CUSTOM_ICONS),
+                    false, mObserver);
+            mContext.getContentResolver().registerContentObserver(
+                    Settings.System.getUriFor(
+                    Settings.System.LOCK_SCREEN_ICON_COLOR), false, mObserver);
+        }
+        fetchTargets();
+    }
+
+    private ContentObserver mObserver = new ContentObserver(null) {
+        @Override
+        public void onChange(boolean selfChange, Uri uri) {
+            fetchTargets();
+            if (mListener != null) {
+                mListener.onChange();
+            }
+        }
+    };
+
+    public static class TargetInfo {
+        public Drawable icon;
+        public boolean colorizeCustomIcons;
+        public String uri;
+        public TargetInfo(Drawable icon, boolean colorizeCustomIcons, String uri) {
+            this.icon = icon;
+            this.colorizeCustomIcons = colorizeCustomIcons;
+            this.uri = uri;
+        }
+    }
+
+    private void fetchTargets() {
+        mTargetActivities = Settings.System.getDelimitedStringAsList(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_TARGETS, DELIMITER);
+        ContentResolver resolver = mContext.getContentResolver();
+        int itemsToPad = Shortcuts.values().length - mTargetActivities.size();
+        if (itemsToPad > 0) {
+            for (int i = 0; i < itemsToPad; i++) {
+                mTargetActivities.add(DEFAULT);
+            }
+        }
+        mColorizeCustomIcons = Settings.System.getInt(resolver,
+                Settings.System.LOCK_SCREEN_SHORTCUTS_COLORIZE_CUSTOM_ICONS, 0) == 1;
+        mIconColor = Settings.System.getInt(resolver,
+                Settings.System.LOCK_SCREEN_ICON_COLOR, 0xffffffff);
+    }
+
+    public List<TargetInfo> getDrawablesForTargets() {
+        fetchTargets();
+        List<TargetInfo> result = new ArrayList<TargetInfo>();
+
+        for (int i = 0; i < Shortcuts.values().length; i++) {
+            String activity = mTargetActivities.get(i);
+            Drawable drawable = null;
+
+            if (!TextUtils.isEmpty(activity) && !activity.equals(NONE)) {
+                // No pre-defined action, try to resolve URI
+                try {
+                    Intent intent = Intent.parseUri(activity, 0);
+                    PackageManager pm = mContext.getPackageManager();
+                    ActivityInfo info = intent.resolveActivityInfo(pm,
+                            PackageManager.GET_ACTIVITIES);
+
+                    if (info != null) {
+                        Drawable d = info.loadIcon(pm);
+                        if (mColorizeCustomIcons) {
+                            drawable = new BitmapDrawable(ImageHelper.getColoredBitmap(d, mIconColor));
+                        } else {
+                            drawable = d;
+                        }
+                    }
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                    // Treat as empty
+                }
+            }
+
+            if (drawable == null) {
+                drawable = getDrawableFromSystemUI(i == Shortcuts.LEFT_SHORTCUT.index
+                        ? PHONE_DEFAULT_ICON : CAMERA_DEFAULT_ICON);
+            }
+
+            result.add(new TargetInfo(drawable, mColorizeCustomIcons, activity));
+        }
+        return result;
+    }
+
+    public Drawable getDrawableFromSystemUI(String name) {
+        Resources res;
+        if (mContext.getPackageName().equals(SYSTEM_UI_PKGNAME)) {
+            res = mContext.getResources();
+        } else {
+            if (mSystemUiResources == null) {
+                try {
+                    PackageManager pm = mContext.getPackageManager();
+                    mSystemUiResources = pm.getResourcesForApplication(SYSTEM_UI_PKGNAME);
+                } catch (PackageManager.NameNotFoundException e) {
+                    // ignore
+                }
+            }
+            res = mSystemUiResources;
+        }
+        if (res == null) {
+            return null;
+        }
+        int id = res.getIdentifier(name, "drawable", SYSTEM_UI_PKGNAME);
+        return id > 0 ? res.getDrawable(id) : null;
+    }
+
+    private String getFriendlyActivityName(Intent intent, boolean labelOnly) {
+        PackageManager pm = mContext.getPackageManager();
+        ActivityInfo ai = intent.resolveActivityInfo(pm, PackageManager.GET_ACTIVITIES);
+        String friendlyName = null;
+        if (ai != null) {
+            friendlyName = ai.loadLabel(pm).toString();
+            if (friendlyName == null && !labelOnly) {
+                friendlyName = ai.name;
+            }
+        }
+        return friendlyName != null || labelOnly ? friendlyName : intent.toUri(0);
+    }
+
+    private String getFriendlyShortcutName(Intent intent) {
+        String activityName = getFriendlyActivityName(intent, true);
+        String name = intent.getStringExtra(Intent.EXTRA_SHORTCUT_NAME);
+
+        if (activityName != null && name != null) {
+            return activityName + ": " + name;
+        }
+        return name != null ? name : intent.toUri(0);
+    }
+
+    public String getFriendlyNameForUri(Shortcuts shortcut) {
+        Intent intent = getIntent(shortcut);
+        if (Intent.ACTION_MAIN.equals(intent.getAction())) {
+            return getFriendlyActivityName(intent, false);
+        }
+        return getFriendlyShortcutName(intent);
+    }
+
+    public boolean isTargetCustom(Shortcuts shortcut) {
+        if (mTargetActivities == null || mTargetActivities.isEmpty()) {
+            return false;
+        }
+        String action = mTargetActivities.get(shortcut.index);
+        if (DEFAULT.equals(action)) {
+            return false;
+        }
+
+        return NONE.equals(action) || getIntent(shortcut) != null;
+    }
+
+    public boolean isTargetEmpty(Shortcuts shortcut) {
+        return mTargetActivities != null &&
+                !mTargetActivities.isEmpty() &&
+                mTargetActivities.get(shortcut.index).equals(NONE);
+    }
+
+    public Intent getIntent(Shortcuts shortcut) {
+        Intent intent = null;
+        try {
+            intent = Intent.parseUri(mTargetActivities.get(shortcut.index), 0);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        return intent;
+    }
+
+    public void saveTargets(ArrayList<String> targets) {
+        Settings.System.putListAsDelimitedString(mContext.getContentResolver(),
+                Settings.System.LOCKSCREEN_TARGETS, DELIMITER, targets);
+    }
+}

packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -49,6 +49,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.android.internal.widget.LockPatternUtils;
+import com.android.internal.util.broken.LockscreenShortcutsHelper;
 import com.android.keyguard.KeyguardStatusView;
 import com.android.systemui.R;
 import com.android.systemui.qs.QSContainer;
@@ -1386,7 +1387,8 @@ public class NotificationPanelView extends PanelView implements
         final float w = getMeasuredWidth();
         float region = (w * (1.f/3.f)); // TODO overlay region fraction?
 
-        boolean showQsOverride = false;
+        boolean showQsOverride = isLayoutRtl() ? (x < region) : (w - region < x);
+        showQsOverride = showQsOverride && mStatusBarState == StatusBarState.SHADE;
 
         if (mOneFingerQuickSettingsInterceptMode == ONE_FINGER_QS_INTERCEPT_END) {
             showQsOverride = isLayoutRtl() ? (x < region) : (w - region < x);
@@ -1795,8 +1797,11 @@ public class NotificationPanelView extends PanelView implements
         mLaunchAnimationEndRunnable = null;
         if (start) {
             mKeyguardBottomArea.launchPhone();
-        } else {
+        } else if (!mKeyguardBottomArea.isTargetCustom(
+                LockscreenShortcutsHelper.Shortcuts.RIGHT_SHORTCUT)) {
             mSecureCameraLaunchManager.startSecureCameraLaunch();
+        } else {
+            mKeyguardBottomArea.launchCamera();
         }
         mStatusBar.startLaunchTransitionTimeout();
         mBlockTouches = true;
@@ -1831,9 +1836,9 @@ public class NotificationPanelView extends PanelView implements
         });
         boolean start = getLayoutDirection() == LAYOUT_DIRECTION_RTL ? right : !right;
         if (start) {
-            mStatusBar.onPhoneHintStarted();
+            mStatusBar.onPhoneHintStarted(mKeyguardBottomArea.getLeftHint());
         } else {
-            mStatusBar.onCameraHintStarted();
+            mStatusBar.onCameraHintStarted(mKeyguardBottomArea.getRightHint());
         }
     }
 