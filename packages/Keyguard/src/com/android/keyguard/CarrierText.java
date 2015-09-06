9e51c25e3571a62e1a1ca2a1181a36ca2733676f
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-09-06 14:20:28 +0100

    Status bar carrier: don't show Emergency text if no sim is installed
    Its way too long and takes up the whole status bar
    
    Change-Id: Ib627be6a84106062eb4ec5fcc03cf1261a4b8b69
    
    Conflicts:
    	packages/Keyguard/src/com/android/keyguard/CarrierText.java

--- packages/Keyguard/src/com/android/keyguard/CarrierText.java
+++ packages/Keyguard/src/com/android/keyguard/CarrierText.java
@@ -143,9 +143,7 @@ public class CarrierText extends TextView {
                 // "No SIM card"
                 // Grab the first subscripton, because they all should contain the emergency text,
                 // described above.
-                displayText =  makeCarrierStringOnEmergencyCapable(
-                        getContext().getText(R.string.keyguard_missing_sim_message_short),
-                        mKeyguardUpdateMonitor.getTelephonyPlmn(subs.get(0).getSubscriptionId()));
+                displayText =  null;
             } else {
                 // We don't have a SubscriptionInfo to get the emergency calls only from.
                 // Grab it from the old sticky broadcast if possible instead. We can use it
@@ -167,8 +165,7 @@ public class CarrierText extends TextView {
                     if (DEBUG) Log.d(TAG, "Getting plmn/spn sticky brdcst " + plmn + "/" + spn);
                     text = concatenate(plmn, spn);
                 }
-                displayText =  makeCarrierStringOnEmergencyCapable(
-                        getContext().getText(R.string.keyguard_missing_sim_message_short), text);
+                displayText =  null;
             }
         }
         setText(displayText);
