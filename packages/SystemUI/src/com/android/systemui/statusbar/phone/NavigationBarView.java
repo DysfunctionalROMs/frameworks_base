3e26459ca1dc5ee690d882204b0adf9df5a6720c
Danesh M <daneshm90@gmail.com>
2015-11-09 11:23:00 -0800

    SystemUI : Fix now on tap home button long press
    Change-Id: Ibd4d8e62d82c43c53bb61fc5ee92053d0f50bc38

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -137,6 +137,7 @@ public class NavigationBarView extends LinearLayout {
     private OnTouchListener mRecentsPreloadListener;
     private OnTouchListener mHomeSearchActionListener;
     private OnLongClickListener mRecentsBackListener;
+    private OnLongClickListener mLongPressHomeListener;
 
     private SettingsObserver mSettingsObserver;
     private boolean mShowDpadArrowKeys;
@@ -733,11 +734,13 @@ public class NavigationBarView extends LinearLayout {
     }
 
     void setListeners(OnClickListener recentsClickListener, OnTouchListener recentsPreloadListener,
-                      OnLongClickListener recentsBackListener, OnTouchListener homeSearchActionListener) {
+                      OnLongClickListener recentsBackListener, OnTouchListener homeSearchActionListener,
+                      OnLongClickListener longPressHomeListener) {
         mRecentsClickListener = recentsClickListener;
         mRecentsPreloadListener = recentsPreloadListener;
         mHomeSearchActionListener = homeSearchActionListener;
         mRecentsBackListener = recentsBackListener;
+        mLongPressHomeListener = longPressHomeListener;
         updateButtonListeners();
     }
 
@@ -749,6 +752,8 @@ public class NavigationBarView extends LinearLayout {
             if (button instanceof KeyButtonView) {
                 button.setOnClickListener(null);
                 button.setOnTouchListener(null);
+                button.setLongClickable(false);
+                button.setOnLongClickListener(null);
             }
         }
     }
@@ -769,6 +774,8 @@ public class NavigationBarView extends LinearLayout {
         View homeView = mCurrentView.findViewWithTag(NavbarEditor.NAVBAR_HOME);
         if (homeView != null) {
             homeView.setOnTouchListener(mHomeSearchActionListener);
+            homeView.setLongClickable(true);
+            homeView.setOnLongClickListener(mLongPressHomeListener);
         }
         View powerView = mCurrentView.findViewWithTag(NavbarEditor.NAVBAR_POWER);
         if (powerView != null) {
