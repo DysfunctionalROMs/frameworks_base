700af898be3fbf8f2195c6dc503beeb33669da6b
InkSlinger666 <kengreenwood420666@gmail.com>
2015-11-17 16:11:35 -0600

    Revert "SystemUI: make long pressing recent switch to last app"
    This reverts commit 6bded1194ed338d95d7527ef50864bfb6cd9fa6b.
    
    Conflicts:
    	packages/SystemUI/res/anim/last_app_in.xml
    	packages/SystemUI/res/anim/last_app_out.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
@@ -49,7 +49,6 @@ public class KeyButtonView extends ImageView {
     private boolean mSupportsLongpress = true;
     private AudioManager mAudioManager;
     private boolean mGestureAborted;
-    private boolean mPerformedLongClick;
 
     private final Runnable mCheckLongPress = new Runnable() {
         public void run() {
@@ -57,7 +56,6 @@ public class KeyButtonView extends ImageView {
                 // Log.d("KeyButtonView", "longpressed: " + this);
                 if (isLongClickable()) {
                     // Just an old-fashioned ImageView
-                    mPerformedLongClick = true;
                     performLongClick();
                 } else if (mSupportsLongpress) {
                     sendEvent(KeyEvent.ACTION_DOWN, KeyEvent.FLAG_LONG_PRESS);
@@ -177,12 +175,11 @@ public class KeyButtonView extends ImageView {
                     }
                 } else {
                     // no key code, just a regular ImageView
-                    if (doIt && !mPerformedLongClick) {
+                    if (doIt) {
                         performClick();
                     }
                 }
                 removeCallbacks(mCheckLongPress);
-                mPerformedLongClick = false;
                 break;
         }
 
