73cfce4bbc81532e4e999c45e93cf7dff89c9ba6
Alexander Martinz <eviscerationls@gmail.com>
2015-11-22 18:28:10 +0100

    Tune up dexopt dialog
      * pass application info, current and total number of packages to the
        boot message interface
      * turn the dialog into a full screen dialog with custom view
    
      * awesome logo in the middle, overlay able (in case we need to rebrand)
          - core/res/res/drawable/dexopt_brand_logo.xml
      * display app icon, dexopt message and package name of the processed package
      * show a progressbar on the bottom, showing the current progress
    
      * disabled app icon loading for low ram devices by default, unless
        high end gfx is forced or "persist.sys.dexopt.no_icon" is set to false
    
    Change-Id: Id07b21b459f8508cfe688139c2afbe27dc4e243a
    Signed-off-by: Alexander Martinz <eviscerationls@gmail.com>
    
    Conflicts:
    	core/res/res/values/custom_symbols.xml
    	services/core/java/com/android/server/pm/PackageManagerService.java
    	services/core/java/com/android/server/policy/PhoneWindowManager.java

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -6095,8 +6095,7 @@ public class PackageManagerService extends IPackageManager.Stub {
                     if (!isFirstBoot()) {
                         try {
                             ActivityManagerNative.getDefault().showBootMessage(
-                                    mContext.getResources().getString(
-                                            R.string.android_upgrading_fstrim), true);
+                                    null, Integer.MIN_VALUE + 1, Integer.MIN_VALUE + 1, true);
                         } catch (RemoteException e) {
                         }
                     }
@@ -6233,9 +6232,8 @@ public class PackageManagerService extends IPackageManager.Stub {
                 }
                 mPolicy.setPackageName((String) (ai != null ? mContext.getPackageManager().getApplicationLabel(ai) : pkg.packageName));
 
-                ActivityManagerNative.getDefault().showBootMessage(
-                        mContext.getResources().getString(R.string.android_upgrading_apk,
-                                curr, total), true);
+                ActivityManagerNative.getDefault().showBootMessage(pkg.applicationInfo,
+                    curr, total, true);
             } catch (RemoteException e) {
             }
         }
