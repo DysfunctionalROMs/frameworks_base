27d30337179871f925a4c0a498296a3d83861094
beanstown106 <nbenis106@gmail.com>
2015-12-04 18:06:36 -0500

    Volume down in silent for dnd toggle *defaults to off *allows to be able to toggle wheter volume down in silent activates dnd
    Conflicts:
    	core/java/android/provider/Settings.java
    	services/core/java/com/android/server/notification/ZenModeHelper.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3945,6 +3945,12 @@ public final class Settings {
          */
         public static final String VOLUME_LINK_NOTIFICATION = "volume_link_notification";
 
+        /**
+         * Boolean value whether volume down in silent can enter zen mode
+         * @hide
+         */
+        public static final String VOLUME_DOWN_ZEN = "volume_down_zen";
+
         /** 
          *
          * @hide
