324504a005695023dd9722c3a98e7068ac92fb82
InkSlinger666 <kengreenwood420666@gmail.com>
2016-04-04 15:36:54 -0500

    Revert "Only window position changed, update window crop."
    This reverts commit 67f8355e38c44d12fbdaea3a0278933ca4ce7a7c.
    
    Conflicts:
    	services/core/java/com/android/server/wm/WindowStateAnimator.java

--- services/core/java/com/android/server/wm/WindowStateAnimator.java
+++ services/core/java/com/android/server/wm/WindowStateAnimator.java
@@ -1430,7 +1430,6 @@ class WindowStateAnimator {
                 if (WindowManagerService.SHOW_TRANSACTIONS) WindowManagerService.logSurface(w,
                         "POS " + left + ", " + top, null);
                 mSurfaceControl.setPosition(left, top);
-                updateSurfaceWindowCrop(recoveringMemory);
                 if (mSurfaceControlBlur != null) {
                     mSurfaceControlBlur.setPosition(left, top);
                 }
@@ -1459,7 +1458,6 @@ class WindowStateAnimator {
                 mSurfaceControl.setMatrix(
                         mDsDx * w.mHScale, mDtDx * w.mVScale,
                         mDsDy * w.mHScale, mDtDy * w.mVScale);
-                updateSurfaceWindowCrop(recoveringMemory);
                 mAnimator.setPendingLayoutChanges(w.getDisplayId(),
                         WindowManagerPolicy.FINISH_LAYOUT_REDO_WALLPAPER);
                 if ((w.mAttrs.flags & LayoutParams.FLAG_DIM_BEHIND) != 0) {
@@ -1486,7 +1484,7 @@ class WindowStateAnimator {
             }
         }
 
-        //updateSurfaceWindowCrop(recoveringMemory);
+        updateSurfaceWindowCrop(recoveringMemory);
     }
 
     public void prepareSurfaceLocked(final boolean recoveringMemory) {
