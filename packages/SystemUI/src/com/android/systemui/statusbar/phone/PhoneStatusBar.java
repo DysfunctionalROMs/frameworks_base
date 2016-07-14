654a60e6eb309596d9fe6d9344d41537bfe38a2e
InkSlinger666 <kengreenwood420666@gmail.com>
2016-07-14 17:54:42 -0500

    Fix lockscreen wallpaper to show custom ls wall when album art is disabled And add blur check
    Change-Id: Ife14d4b825313c2cdba1a8f848f0bc3b7168b9b1

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2181,7 +2181,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         }
 
         // apply user lockscreen image
-        if (mMediaMetadata == null && backdropBitmap == null) {
+        if (backdropBitmap == null && mBlurredImage == null) {
             backdropBitmap = mKeyguardWallpaper;
         }
 
