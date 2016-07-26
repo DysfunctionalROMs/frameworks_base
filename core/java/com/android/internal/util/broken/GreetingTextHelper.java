XXMrHyde/android_frameworks_base

Status bar: Greeting, (1/2):
Ported from Darkkat 5.1:
Status bar: Greeting, (1/2):
https://github.com/XXMrHyde/android_frameworks_base/commit/68f9ac5361efd56929d7fa2d52762c319e560efb

Status bar, Greeting: improvements, (1/2):
https://github.com/XXMrHyde/android_frameworks_base/commit/60fe167a73f421837fa81ae48177bfe9901e5c97

Included:
- Show greeting:
  - Always, (every time the decice gets unlocked from lock screen)
  - Once, , (the first time the decice gets unlocked from lock screen after boot)
  - never
- Custom text
- Duration, (the time between fade in and fade out)
- Show preview
- Text color
- Text color dark mode

Changes:
- Use the default alpha fade in/out animations,
  (on 6.0 there is no notification ticker,
  to be consistent use the same animations the status bar is using, too)
- Add color dark mode

core/java/com/android/internal/util/darkkat/GreetingTextHelper.java
@@ -1,0 +1,43 @@
+/*
+* Copyright (C) 2015 DarkKat
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.darkkat;
+
+import android.content.Context;
+import android.os.Build;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class GreetingTextHelper {
+
+    public static String getDefaultGreetingText(Context context) {
+
+        final String welcome = context.getResources().getString(
+                com.android.internal.R.string.default_greeting_welcome);
+        Date date = new Date(Build.TIME); 
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT+1"));
+        String buildDate = sdf.format(date);
+
+        String defaultGreetingText =
+                welcome + " " + Build.VERSION.RELEASE
+                + " (" + Build.ID + "), " + buildDate;
+
+        return defaultGreetingText;
+    }
+}

DysfunctionalROMs/frameworks_base

Change-Id: I7dae70a5603a65f0fe27211a4256748bcb6ec289

core/java/com/android/internal/util/broken/GreetingTextHelper.java
@@ -1,0 +1,43 @@
+/*
+* Copyright (C) 2015 DarkKat
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package com.android.internal.util.broken;
+
+import android.content.Context;
+import android.os.Build;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class GreetingTextHelper {
+
+    public static String getDefaultGreetingText(Context context) {
+
+        final String welcome = context.getResources().getString(
+                com.android.internal.R.string.default_greeting_welcome);
+        Date date = new Date(Build.TIME);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT+1"));
+        String buildDate = sdf.format(date);
+
+        String defaultGreetingText =
+                welcome + " " + Build.VERSION.RELEASE
+                + " (" + Build.ID + "), " + buildDate;
+
+        return defaultGreetingText;
+    }
+}