voidz777-pure/android_frameworks_base

OMS: Themes should be dynamically theming the power menu [7/8]
OMS introduced a different way of loading resources into apps, and this new behavior is still buggy. Power menu wasn't getting themed with
our working oms code.

This commit:
 - Picks the following commits from CM
 - Introduces a new helper class: ThemeUtils
 - A lot of name refactoring, mainly uiContext --> mUiContext
 - We can't check (yet) for theme change as CMTE, so I had to drop some if (mContext != null) guards. I didn't notice any performance difference.
 - Both stock and themes should work

Reworked for OMS by @KreAch3R

This is a combination of the following commits:

Show reboot dialog using themed resources: https://github.com/CyanogenMod/android_frameworks_base/commit/d8e138b06abf4147a37d2c3e8f1e95a7e9b52e5c
by 0xD34D

Themes: Theme power off and reboot dialog: https://github.com/CyanogenMod/android_frameworks_base/commit/f7fc9ceefa1e9cad133d694d93f43e3abf18406e
by Altaf-Mahdi
This patch allows theming of the dialog which
comes after the power menu

Distilled changes from these:
https://github.com/CyanogenMod/android_frameworks_base/commit/a4e229c7c09f39ee54df4a150826cab9e435f890
and
https://github.com/CyanogenMod/cm_platform_sdk/blame/cm-13.0/sdk/src/java/org/cyanogenmod/internal/util/ThemeUtils.java

Change-Id: Ic82fcadeede4c4352db380b3f31f516c7b10877b

services/core/java/com/android/server/policy/GlobalActions.java
@@ -21,6 +21,7 @@ import com.android.internal.app.AlertController.AlertParams;
 import com.android.internal.telephony.TelephonyIntents;
 import com.android.internal.telephony.TelephonyProperties;
 import com.android.internal.R;
+import com.android.internal.util.ThemeUtils;
 import com.android.internal.util.UserIcons;
 import com.android.internal.widget.LockPatternUtils;
 
@@ -113,6 +114,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
     private final Context mContext;
     private final WindowManagerFuncs mWindowManagerFuncs;
+    private Context mUiContext;
     private final AudioManager mAudioManager;
     private final IDreamManager mDreamManager;
 
@@ -221,6 +223,12 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
             mDialog.show();
             mDialog.getWindow().getDecorView().setSystemUiVisibility(View.STATUS_BAR_DISABLE_EXPAND);
         }
+    }
+
+    private Context getUiContext() {
+        mUiContext = ThemeUtils.createUiContext(mContext);
+        mUiContext.setTheme(android.R.style.Theme_DeviceDefault_Light_DarkActionBar);
+        return mUiContext != null ? mUiContext : mContext;
     }
 
     /**
@@ -341,12 +349,12 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         mAdapter = new MyAdapter();
 
-        AlertParams params = new AlertParams(mContext);
+        AlertParams params = new AlertParams(getUiContext());
         params.mAdapter = mAdapter;
         params.mOnClickListener = this;
         params.mForceInverseBackground = true;
 
-        GlobalActionsDialog dialog = new GlobalActionsDialog(mContext, params);
+        GlobalActionsDialog dialog = new GlobalActionsDialog(getUiContext(), params);
         dialog.setCanceledOnTouchOutside(false); // Handled by the custom class.
 
         dialog.getListView().setItemsCanFocus(true);
@@ -963,7 +971,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         public View getView(int position, View convertView, ViewGroup parent) {
             Action action = getItem(position);
-            return action.create(mContext, convertView, parent, LayoutInflater.from(mContext));
+            final Context context = getUiContext();
+            return action.create(context, convertView, parent, LayoutInflater.from(context));
         }
     }
 

DysfunctionalROMs/frameworks_base

Conflicts:
	services/core/java/com/android/server/power/ShutdownThread.java

services/core/java/com/android/server/policy/GlobalActions.java
@@ -21,6 +21,7 @@ import com.android.internal.app.AlertController.AlertParams;
 import com.android.internal.telephony.TelephonyIntents;
 import com.android.internal.telephony.TelephonyProperties;
 import com.android.internal.R;
+import com.android.internal.util.ThemeUtils;
 import com.android.internal.widget.LockPatternUtils;
 
 import android.app.ActivityManager;
@@ -106,6 +107,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
     private final Context mContext;
     private final WindowManagerFuncs mWindowManagerFuncs;
+    private Context mUiContext;
     private final AudioManager mAudioManager;
     private final IDreamManager mDreamManager;
 
@@ -287,6 +289,12 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         double dAlpha = mPowerMenuAlpha / 100.0;
         float alpha = (float) dAlpha;
         return alpha;
+	}
+
+    private Context getUiContext() {
+        mUiContext = ThemeUtils.createUiContext(mContext);
+        mUiContext.setTheme(android.R.style.Theme_Material_DayNight_Dialog_Alert);
+        return mUiContext != null ? mUiContext : mContext;
     }
 
     /**
@@ -428,12 +436,12 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         mAdapter = new MyAdapter();
 
-        AlertParams params = new AlertParams(mContext);
+        AlertParams params = new AlertParams(getUiContext());
         params.mAdapter = mAdapter;
         params.mOnClickListener = this;
         params.mForceInverseBackground = true;
 
-        GlobalActionsDialog dialog = new GlobalActionsDialog(mContext, params);
+        GlobalActionsDialog dialog = new GlobalActionsDialog(getUiContext(), params);
         dialog.setCanceledOnTouchOutside(false); // Handled by the custom class.
 
         dialog.getListView().setItemsCanFocus(true);
@@ -922,7 +930,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
 
         public View getView(int position, View convertView, ViewGroup parent) {
             Action action = getItem(position);
-            return action.create(mContext, convertView, parent, LayoutInflater.from(mContext));
+            final Context context = getUiContext();
+            return action.create(context, convertView, parent, LayoutInflater.from(context));
         }
     }
 
@@ -1387,7 +1396,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         private boolean mCancelOnUp;
 
         public GlobalActionsDialog(Context context, AlertParams params) {
-            super(context, com.android.internal.R.style.Theme_Material_DayNight_Dialog_Alert);
+            super(context);
             mContext = getContext();
             mAlert = new AlertController(mContext, this, getWindow());
             mAdapter = (MyAdapter) params.mAdapter;